1f586909bccccff144ba5edbba496e74d77115b4
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-04-13 14:22:58 +0100

    Updates

---
+++ precache-manifest.89eba66ee569929ad0054d07b3bfe448.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e65a02acae06097f1bbff4ded250cf",
+    "url": "/react-quze-search-app/index.html"
+  },
+  {
+    "revision": "9ba588701de76ede6a85",
+    "url": "/react-quze-search-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "eb1fff0181df85e0001c",
+    "url": "/react-quze-search-app/static/css/main.55065afd.chunk.css"
+  },
+  {
+    "revision": "9ba588701de76ede6a85",
+    "url": "/react-quze-search-app/static/js/2.d797ed0f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-quze-search-app/static/js/2.d797ed0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1fff0181df85e0001c",
+    "url": "/react-quze-search-app/static/js/main.728a08a9.chunk.js"
+  },
+  {
+    "revision": "6839b1dcbf75ed23bcaf",
+    "url": "/react-quze-search-app/static/js/runtime-main.3324e855.js"
+  }
+]);
