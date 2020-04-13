8ec7e96616b6647e18b9d841ff0a0456acf62d0d
Tina Oluwabukola <oluwabukolatina@gmail.com>
2020-04-13 08:53:20 +0100

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,49 +6,108 @@ const Home = () => {
     return (
        <Fragment>
            <NavBar/>
-           <Container>
-               <CardDeck>
-                   <Card>
-                       <Card.Img variant="top" src="holder.js/100px160" />
-                       <Card.Body>
-                           <Card.Title>Card title</Card.Title>
-                           <Card.Text>
-                               This is a wider card with supporting text below as a natural lead-in to
-                               additional content. This content is a little bit longer.
-                           </Card.Text>
-                       </Card.Body>
-                       <Card.Footer>
-                           <small className="text-muted">Last updated 3 mins ago</small>
-                       </Card.Footer>
-                   </Card>
-                   <Card>
-                       <Card.Img variant="top" src="holder.js/100px160" />
-                       <Card.Body>
-                           <Card.Title>Card title</Card.Title>
-                           <Card.Text>
-                               This card has supporting text below as a natural lead-in to additional
-                               content.{' '}
-                           </Card.Text>
-                       </Card.Body>
-                       <Card.Footer>
-                           <small className="text-muted">Last updated 3 mins ago</small>
-                       </Card.Footer>
-                   </Card>
-                   <Card>
-                       <Card.Img variant="top" src="holder.js/100px160" />
-                       <Card.Body>
-                           <Card.Title>Card title</Card.Title>
-                           <Card.Text>
-                               This is a wider card with supporting text below as a natural lead-in to
-                               additional content. This card has even longer content than the first to
-                               show that equal height action.
-                           </Card.Text>
-                       </Card.Body>
-                       <Card.Footer>
-                           <small className="text-muted">Last updated 3 mins ago</small>
-                       </Card.Footer>
-                   </Card>
-               </CardDeck>
+           <Container style={{    marginTop: '3%'}}>
+               <div style={{display: 'flex', width: '100%'}}>
+                   <CardDeck>
+                       <div style={{ width: '25%'}}>
+                           <Card style={{marginBottom: '9%'}}>
+                               <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                               <Card.Body>
+                                   <Card.Title>Card title</Card.Title>
+                                   <Card.Text>
+                                       This card has supporting text below as a natural lead-in to additional
+                                       content.{' '}
+                                   </Card.Text>
+                               </Card.Body>
+                               <Card.Footer>
+                                   <small className="text-muted">Last updated 3 mins ago</small>
+                               </Card.Footer>
+                           </Card>
+                       </div>
+
+                       <div style={{ width: '25%'}}>
+                           <Card>
+                               <Card.Img variant="top" src="holder.js/100px160" />
+                               <Card.Body>
+                                   <Card.Title>Card title</Card.Title>
+                                   <Card.Text>
+                                       This card has supporting text below as a natural lead-in to additional
+                                       content.{' '}
+                                   </Card.Text>
+                               </Card.Body>
+                               <Card.Footer>
+                                   <small className="text-muted">Last updated 3 mins ago</small>
+                               </Card.Footer>
+                           </Card>
+                       </div>
+
+                       <div style={{ width: '25%'}}>
+                           <Card>
+                               <Card.Img variant="top" src="holder.js/100px160" />
+                               <Card.Body>
+                                   <Card.Title>Card title</Card.Title>
+                                   <Card.Text>
+                                       This card has supporting text below as a natural lead-in to additional
+                                       content.{' '}
+                                   </Card.Text>
+                               </Card.Body>
+                               <Card.Footer>
+                                   <small className="text-muted">Last updated 3 mins ago</small>
+                               </Card.Footer>
+                           </Card>
+                       </div>
+
+                       <div style={{ width: '25%'}}>
+                           <Card>
+                               <Card.Img variant="top" src="holder.js/100px160" />
+                               <Card.Body>
+                                   <Card.Title>Card title</Card.Title>
+                                   <Card.Text>
+                                       This card has supporting text below as a natural lead-in to additional
+                                       content.{' '}
+                                   </Card.Text>
+                               </Card.Body>
+                               <Card.Footer>
+                                   <small className="text-muted">Last updated 3 mins ago</small>
+                               </Card.Footer>
+                           </Card>
+                       </div>
+
+                       <div style={{ width: '25%'}}>
+                           <Card>
+                               <Card.Img variant="top" src="holder.js/100px160" />
+                               <Card.Body>
+                                   <Card.Title>Card title</Card.Title>
+                                   <Card.Text>
+                                       This card has supporting text below as a natural lead-in to additional
+                                       content.{' '}
+                                   </Card.Text>
+                               </Card.Body>
+                               <Card.Footer>
+                                   <small className="text-muted">Last updated 3 mins ago</small>
+                               </Card.Footer>
+                           </Card>
+                       </div>
+
+                       <div style={{ width: '25%'}}>
+                           <Card>
+                               <Card.Img variant="top" src="holder.js/100px160" />
+                               <Card.Body>
+                                   <Card.Title>Card title</Card.Title>
+                                   <Card.Text>
+                                       This card has supporting text below as a natural lead-in to additional
+                                       content.{' '}
+                                   </Card.Text>
+                               </Card.Body>
+                               <Card.Footer>
+                                   <small className="text-muted">Last updated 3 mins ago</small>
+                               </Card.Footer>
+                           </Card>
+                       </div>
+
+
+                   </CardDeck>
+               </div>
            </Container>
        </Fragment>
     );
--- src/layouts/NavBar.js
+++ src/layouts/NavBar.js
@@ -3,17 +3,27 @@ import {Form, Nav, Navbar, FormControl, Button} from "react-bootstrap";
 
 const NavBar = () => {
     return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-info">Search</Button>
-            </Form>
+        <Navbar bg="light" variant="dark" style={{margin: '1%'}}>
+            <div style={{width: '100%', display: 'flex'}}>
+                <div style={{width: '50%', }}>
+                    <Navbar.Brand href="#home" style={{color: 'black'}}>Navbar</Navbar.Brand>
+
+                </div>
+                <div style={{width: '50%'}}>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home" style={{width: '25%', color: 'black'}} >Trending</Nav.Link>
+                        <Nav.Link href="#features" style={{width: '25%', color: 'black'}}>Mission</Nav.Link>
+                        <Nav.Link href="#features" style={{width: '25%', color: 'black'}}>Partners</Nav.Link>
+                        <Nav.Link href="#features" style={{width: '25%', color: 'black'}}>Contact Us</Nav.Link>
+
+                    </Nav>
+
+                </div>
+            </div>
+            {/*<Form inline>*/}
+            {/*    <FormControl type="text" placeholder="Search" className="mr-sm-2" />*/}
+            {/*    <Button variant="outline-info">Search</Button>*/}
+            {/*</Form>*/}
         </Navbar>
     );
 };
